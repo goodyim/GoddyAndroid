05fc569d02d7f0cc55f0aedfad8ae5e4f95d5b0d
Valentun Sergeev <karbon4ic@gmail.com>
2017-08-21 08:31:11 +0600

    Change package name

--- app/src/androidTest/java/com/goddy/goddyandroid/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/goddy/goddyandroid/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.valentun.goddyandroid;
+package com.goddy.goddyandroid;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.valentun.goddyandroid", appContext.getPackageName());
+        assertEquals("com.goddy.goddyandroid", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/goddy/goddyandroid/RootActivity.java
+++ app/src/main/java/com/goddy/goddyandroid/RootActivity.java
@@ -1,4 +1,4 @@
-package com.valentun.goddyandroid;
+package com.goddy.goddyandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
