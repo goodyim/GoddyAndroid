1ad24ceb50d1f4d0d0f8ff2bed0b8cc13cd8958c
Valentun Sergeev <karbon4ic@gmail.com>
2017-09-14 18:37:32 +0600

    Russian localization

--- app/src/main/java/im/goody/android/Constants.java
+++ app/src/main/java/im/goody/android/Constants.java
@@ -21,33 +21,6 @@ public class Constants {
             R.color.primary
     };
 
-    public static class Welcome {
-        public static final int PAGES_COUNT = 3;
-        public static final int[] TITLES = {
-                R.string.welcome_title_1,
-                R.string.welcome_title_2,
-                R.string.welcome_title_3
-        };
-
-        public static final int[] DESCRIPTIONS = {
-                R.string.welcome_description_1,
-                R.string.welcome_description_2,
-                R.string.welcome_description_3
-        };
-
-        public static final int[] COLORS = {
-                R.color.accent,
-                R.color.primary,
-                R.color.auth_facebook_background
-        };
-
-        public static final int[] ICONS = {
-                R.drawable.ic_add,
-                R.drawable.ic_comment,
-                R.drawable.ic_heart
-        };
-    }
-
     public static class Pattern {
         public static final String OPEN_MAP = "geo:0,0?q=%s";
     }
