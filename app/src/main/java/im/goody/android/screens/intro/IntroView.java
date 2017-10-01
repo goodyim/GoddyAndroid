3f27d653ef0513dd8d8955aeb0ad77285a046e0f
DmiSb <DmiSb@inbox.ru>
2017-10-01 20:41:57 +0500

    Refactoring Intro Screen

--- app/src/main/java/im/goody/android/screens/intro/IntroView.java
+++ app/src/main/java/im/goody/android/screens/intro/IntroView.java
@@ -1,8 +1,10 @@
 package im.goody.android.screens.intro;
 
 import android.content.Context;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 
+import im.goody.android.R;
 import im.goody.android.core.BaseView;
 import im.goody.android.databinding.ScreenIntroBinding;
 
@@ -27,10 +29,28 @@ public class IntroView extends BaseView<IntroController, ScreenIntroBinding> {
     @Override
     protected void onAttached() {
         binding.introNext.setOnClickListener(v -> controller.close());
+        binding.introPager.addOnPageChangeListener(pageChangeListener);
     }
 
     @Override
     protected void onDetached() {
-
+        binding.introPager.removeOnPageChangeListener(pageChangeListener);
     }
+
+    private final ViewPager.OnPageChangeListener pageChangeListener =
+            new ViewPager.OnPageChangeListener() {
+                @Override
+                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+                @Override
+                public void onPageSelected(int position) {
+                    if (position == adapter.getCount() - 1)
+                        binding.introNext.setText(R.string.intro_start);
+                    else
+                        binding.introNext.setText(R.string.intro_close);
+                }
+
+                @Override
+                public void onPageScrollStateChanged(int state) {}
+            };
 }
